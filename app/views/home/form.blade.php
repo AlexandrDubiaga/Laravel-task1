29fa4cd6cdc3190bea8fba39adbb7fc2d1bea5cd
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 20:33:34 +0300

    Update form.blade.php

--- app/views/home/form.blade.php
+++ app/views/home/form.blade.php
@@ -1,7 +1,7 @@
 <html>
 <body>
 Hello {{{$name}}}
-<form method="POST" action="">
+<form method="POST" action="{{URL::to('index')}}">
 <p>name</p>
 <input  type="text" name="name" /><br>
 <p>message</p>
