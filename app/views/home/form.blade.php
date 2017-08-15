d7dcc280f88b1f1c995aafc4b43cf534b52b93c5
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 19:55:47 +0300

    Create form.blade.php

---
+++ app/views/home/form.blade.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<form method="POST" action="{{URL::to('formAct/form')}}">
+<input  type="text" name="name" />
+<input  type="password" name="name" />
+<input type="submit" val="go">
+</form>
+</body>
+</html>
