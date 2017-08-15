33649229c25852139431caac505b596e52f3a081
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 20:39:01 +0300

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,7 @@ Route::get('/', function()
 	return Redirect::to('/home');
 });
 Route::get('/home/{variable?}','HomeController@showLinks');
+Route::get('/form/home/{variable?}','HomeController@showLinks');
 Route::get('/form/{name?}','HomeController@showForm');
 Route::get('/articles','HomeController@showArticles');
 
