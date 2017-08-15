f076b896885685d8fdea61f5c89885ec58945bc5
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 20:15:44 +0300

    Update index.blade.php

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -2,7 +2,7 @@
   <body>
   <h1>{{{$variable}}}<h1>
   <a href="{{URL::to('form')}}">Form</a>
-   <a href="{{URL::to('actions)}}">Actions</a>
+   <a href="{{URL::to('articles')}}">Articles</a>
   <body>
 
 </html>
