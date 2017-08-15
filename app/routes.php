070e40b9eba06702fdaaf9ee307e0d0410f2baae
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 20:02:30 +0300

    Update routes.php

--- app/routes.php
+++ app/routes.php
@@ -5,6 +5,6 @@ Route::get('/', function()
 	return Redirect::to('/home');
 });
 Route::get('/home/{variable?}','HomeController@showLinks');
-Route::get('/form/{var?}','HomeController@showActions');
+Route::get('/form','HomeController@showActions');
 Route::controller('formAct','HomeController');
 
