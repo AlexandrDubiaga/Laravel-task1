2d67e71c402bfb519a3db1e09fb4728e8c2c5310
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 17:38:13 +0300

    Update HomeController.php

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,7 @@
 <?php
 
 class HomeController extends BaseController {
-	public function showLinks($variable)
+	public function showLinks($variable=null)
 	{
 		return View::make('home.index',array('variable'=>$variable));
 	}
