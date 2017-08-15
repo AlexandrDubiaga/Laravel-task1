f07f819680219c2626d3c2918be3464dddf9fdb0
AlexandrDubiaga <alexandr.dubiaga@gmail.com>
2017-08-15 20:11:22 +0300

    Update form.blade.php

--- app/views/home/form.blade.php
+++ app/views/home/form.blade.php
@@ -1,6 +1,6 @@
 <html>
 <body>
-<form method="POST" action="{{URL::to('formAct/form')}}">
+<form method="POST" action="">
 <input  type="text" name="name" />
 <input  type="password" name="name" />
 <input type="submit" val="go">
